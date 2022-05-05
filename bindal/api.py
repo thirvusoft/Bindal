812f150c68d44633b33a0ad3c3ecc01f4ff71185
AkashMunikoti <akashmunikoti@gmail.com>
2022-05-05 11:10:02 +0530

    api.py for series

--- bindal/api.py
+++ bindal/api.py
@@ -21,11 +21,11 @@ import ast
 def get_stock_qty(item_code,warehouse):
     print("entered in get_stock_qty function")
     qty = frappe.db.sql("""select concat_ws(" ", posting_date, posting_time) as date,qty_after_transaction from `tabStock Ledger Entry` where item_code='"""+item_code+"""' and warehouse='"""+warehouse+"""' order by posting_date,posting_time """, as_dict=1)
-    print("qty",qty)
+    print("quantity",qty)
     return qty
 
 @frappe.whitelist()
 def fetch_acc_dimension_company():
-	company=frappe.db.sql("""select company from `tabAccounting Dimension Detail` as adetail join `tabAccounting Dimension` as ad on ad.name = adetail.parent""", as_dict=1)
-	print("company_details",company)
-	return company
+    company=frappe.db.sql("""select company from `tabAccounting Dimension Detail` as adetail join `tabAccounting Dimension` as ad on ad.name = adetail.parent""", as_dict=1)
+    print("company_details",company)
+    return company

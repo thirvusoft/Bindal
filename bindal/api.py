a3acd4bc85b10a3365449b57eaab4b15d658e9a9
AkashMunikoti <akashmunikoti@gmail.com>
2022-08-11 19:38:57 +0530

    email modified

--- bindal/api.py
+++ bindal/api.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import frappe
+from datetime import datetime,date
 from datetime import datetime
 
 @frappe.whitelist()
@@ -42,25 +43,78 @@ def fetch_acc_dimension_company():
 
 @frappe.whitelist()
 def send_email_for_due_date():
-    if frappe.db.exists('Email Template','Send Mail'):
-        next_action_template = frappe.get_doc('Email Template', "Send Mail")
-        next_actions_list = frappe.db.get_list('ToDo',fields=['*'],filters={"date":frappe.utils.nowdate(),"status":["!=","Closed"]})
-        print("Todo doc details",next_actions_list)
-        print("next_action_template",next_action_template.response)
-        for action in next_actions_list :       
-            message = frappe.render_template(next_action_template.response, action)
+    message = " "
+    next_actions_list = frappe.db.get_list('ToDo',fields=['*'],filters={})
+    for action in next_actions_list :    
+        posting_date= action.date
+        p_date = str(posting_date)
+        now = datetime.now()
+        current_dte = now.strftime("%Y-%m-%d")
+        time_format = '%Y-%m-%d'
+        no_of_days = datetime.strptime(p_date, time_format) - datetime.strptime(current_dte, time_format)
+        owner = action.owner
+        owner = owner.split()
+        for own in owner :
+            full_name = frappe.db.get_list("User",filters={"name":own},fields=["full_name"]) 
+        if no_of_days.days == 0:
+            subject = """ ALERT, Task """+action.pch_subject+""" is due today """+p_date.strftime("%d/%m/%Y")+""" """
+            message = """ Hai """+full_name[0].full_name+""" , <br><br> Task No """+action.name+""" , """+action.pch_subject+""" has been assigned to you and is: 
+            due today """+p_date.strftime("%d/%m/%Y")+""" <br><br> Please ensure that you complete this task and update on ERPNext that you have completed the task. <br><br>
+            Since this task is: due today , please meet Kapil Gupta and/or Ajay Tickoo and discuss what support you need to complete the task today.<br>
+            """                    
             print("message",message)
             recipients= action.pch_recipient_list
             print("recipients",recipients)
-            subject=next_action_template.subject
             print("subject",subject)
             frappe.sendmail(
-                recipients= action.pch_recipient_list,
-                subject=next_action_template.subject,
-                message=message,
-                reference_doctype=action.parenttype,
-                reference_name=action.parent
+            recipients= recipients,
+            subject=subject,
+            message=message
             )
-    else:
-        frappe.msgprint(('Please Configure Email Template'))
-
+        if no_of_days.days < 0:
+            subject = """ ALERT, Task """+action.pch_subject+""" is due on """+p_date.strftime("%d/%m/%Y")+""" """
+            message = """ Hai """+full_name[0].full_name+""" ,<br><br> Task No """+action.name+""" , """+action.pch_subject+""" has been assigned to you and is: 
+            Overdue("""+p_date.strftime("%d/%m/%Y")+""") <br><br>
+            Since this task is: Overdue , please meet Kapil Gupta and/or Ajay Tickoo and discuss what support you need to complete the task today.<br><br>
+            please meet Kapil Gupta and/or Ajay Tickoo today without fail and discuss how you are going to complete the task today. Not completing the task today may invite disciplinary action."""                    
+            print("message",message)
+            recipients= action.pch_recipient_list
+            print("recipients",recipients)
+            print("subject",subject)
+            frappe.sendmail(
+            recipients= recipients,
+            subject=subject,
+            message=message
+            )
+        if no_of_days.days == 1:
+            subject = """ ALERT, Task """+action.pch_subject+""" is due on """+p_date.strftime("%d/%m/%Y")+""" """
+            message = """ Hai """+full_name[0].full_name+""" ,<br><br> 
+            Task No """+action.name+""" , """+action.pch_subject+""" has been assigned to you and is: 
+            due tomorrow ("""+p_date.strftime("%d/%m/%Y")+""") <br><br> 
+            Please ensure that you complete this task and update on ERPNext that you have completed the task.
+            """                    
+            print("message",message)
+            recipients= action.pch_recipient_list
+            print("recipients",recipients)
+            print("subject",subject)
+            frappe.sendmail(
+            recipients= recipients,
+            subject=subject,
+            message=message
+            )
+        if no_of_days.days > 1:
+            subject = """ ALERT, Task """+action.pch_subject+""" is s due on """+p_date.strftime("%d/%m/%Y")+"""  <br><br> """
+            message = """ Hai """+full_name[0].full_name+""" ,<br><br> 
+            Task No """+action.name+""" , """+action.pch_subject+""" has been assigned to you and is: 
+            due on ("""+p_date.strftime("%d/%m/%Y")+""") <br><br> 
+            Please ensure that you complete this task and update on ERPNext that you have completed the task.<br><br> 
+            """                    
+            print("message",message)
+            recipients= action.pch_recipient_list
+            print("recipients",recipients)
+            print("subject",subject)
+            frappe.sendmail(
+            recipients= recipients,
+            subject=subject,
+            message=message
+            )

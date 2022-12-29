d3b14b533a861828e49aaa9d9aa6250569757e23
AkashMunikoti <akashmunikoti@gmail.com>
2022-12-29 12:22:25 +0530

    gate_entry_balance_quantity_report modified

--- bindal/bindal/report/gate_entry_balance_quantity_report/gate_entry_balance_quantity_report.py
+++ bindal/bindal/report/gate_entry_balance_quantity_report/gate_entry_balance_quantity_report.py
@@ -14,38 +14,56 @@ import json
 import ast
 
 sum_data = []
+d = []
 def execute(filters=None):
 	global sum_data
 	columns = []
 	sum_data = []
+	d = []
 	purchase_order = filters.get("purchase_order")
 	print("purchase_order=============",purchase_order)
 	columns = get_columns()
 	data = fetching_po_details(purchase_order)
-	for gate_data in data:
-		if gate_data['name']:
-			item_master = frappe.db.sql("""select over_delivery_receipt_allowance from `tabItem` where item_code='"""+gate_data['item_code']+"""' """, as_dict=1)
-			stock_settings=frappe.get_doc("Stock Settings")
-			result = item_master[0]['over_delivery_receipt_allowance'] - stock_settings.over_delivery_receipt_allowance
-			print("result",result)
-
-			r = (gate_data['po_qty'] * result)/100
-			pending_qty_with_over_del_percentage = gate_data['pending_qty'] + r
-
-			blnce_with_over_delivery_percentage = pending_qty_with_over_del_percentage - gate_data['received_qty']
-
-			sum_data.append([gate_data['item_code'],gate_data['po_qty'],gate_data['uom'],gate_data['pending_qty'],
-			pending_qty_with_over_del_percentage,gate_data['received_qty'],gate_data['stock_uom'],gate_data['balance'],blnce_with_over_delivery_percentage
-			])
-			print("111111",sum_data)
+	d = []
+	for i in data:
+		flag = False
+		index = None
+		for idx,j in enumerate(d):
+			if j["record_number"] == i["record_number"] and j["item_code"] == i["item_code"]:
+				flag = True
+				index = idx
+				break
+		if flag:
+			d[idx]["received_qty"] = d[idx]["received_qty"] + i["received_qty"]
+		else:
+			d.append(i)
+	print("sumdata",d)
+	for gate_data in d:
+		item_master = frappe.db.sql("""select over_delivery_receipt_allowance from `tabItem` where item_code='"""+gate_data['item_code']+"""' """, as_dict=1)
+		stock_settings=frappe.get_doc("Stock Settings")
+		result = item_master[0]['over_delivery_receipt_allowance'] - stock_settings.over_delivery_receipt_allowance
+		print("result",result)
+		r = (gate_data['po_qty'] * result)/100
+		pending_qty_with_over_del_percentage = gate_data['pending_qty'] + r
+		blnce_with_over_delivery_percentage = pending_qty_with_over_del_percentage - gate_data['received_qty']
+		results = 1+(result/100)
+		resultess = gate_data['po_qty'] * results  
+		r_result = resultess - gate_data['received_qty']
+		sum_data.append([gate_data['item_code'],gate_data['po_qty'],gate_data['uom'],
+		gate_data['pending_qty'],pending_qty_with_over_del_percentage,
+		gate_data['received_qty'],gate_data['stock_uom'],
+		gate_data['balance'],r_result
+		])
+	print("result............",sum_data)
 	return columns, sum_data
 
 def fetching_po_details(purchase_order):
 	print(".....",purchase_order)
-	gate_data = frappe.db.sql("""select gt.name,gti.item_code,gti.item_name,gti.uom,gti.po_qty,gti.pending_qty,gti.stock_uom,
-	gti.received_qty,gti.pending_qty - gti.received_qty as balance
-	from `tabGate Entry` as gt inner join `tabPO Item` as gti on gt.name=gti.parent
-	 where gti.record_number='"""+purchase_order+"""' and gt.docstatus=1 """, as_dict=1)
+	gate_data = frappe.db.sql("""select gti.record_number,gti.item_code,gti.item_name,
+	gti.uom,gti.po_qty,gti.pending_qty,gti.stock_uom, gti.received_qty,
+	gti.pending_qty - gti.received_qty as balance 
+	from `tabGate Entry` as gt inner join `tabPO Item` as gti on gt.name=gti.parent  
+	where gti.record_number='"""+purchase_order+"""' and gt.docstatus=1 """, as_dict=1)
 	print("gate_data.....",gate_data)
 	return gate_data
 
@@ -56,9 +74,9 @@ def get_columns():
 		_("Qty")+"::100",
 		_("UOM")+"::100",
 		_("Pending PO Quantity")+"::100",
-		_("Pending PO Quantity with over delivery percentage")+"::100",
-		_("Gate Entry Quantity")+":150",
-		_("UOM")+":150",
+		_("Pending PO Quantity with over delivery percentage")+"::150",
+		_("Gate Entry Quantity")+":100",
+		_("UOM")+":100",
 		_("Balance")+"::100",
 		_("Balance with Over Delivery Percentage")+"::150"
 		  ]

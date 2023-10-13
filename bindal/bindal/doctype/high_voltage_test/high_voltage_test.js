a9278cf03ebfea5bdbaaa2a0eedc2c4fbb5deb5a
Ubuntu <ubuntu@ip-172-31-44-225.ap-south-1.compute.internal>
2023-10-13 08:41:35 +0000

    doctype updation

--- bindal/bindal/doctype/high_voltage_test/high_voltage_test.js
+++ bindal/bindal/doctype/high_voltage_test/high_voltage_test.js
@@ -42,12 +42,66 @@ frappe.ui.form.on('High Voltage Test', {
 			frm.set_value('remark5',"Dissatisfactory")
 		}
 	},
+	leakage6: function(frm) {
+		if(frm.doc.leakage6 == "YES"){
+			frm.set_value('remark6',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark6',"Dissatisfactory")
+		}
+	},
+	leakage7: function(frm) {
+		if(frm.doc.leakage7 == "YES"){
+			frm.set_value('remark7',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark7',"Dissatisfactory")
+		}
+	},
+	leakage8: function(frm) {
+		if(frm.doc.leakage8 == "YES"){
+			frm.set_value('remark8',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark8',"Dissatisfactory")
+		}
+	},
+	leakage9: function(frm) {
+		if(frm.doc.leakage9 == "YES"){
+			frm.set_value('remark9',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark9',"Dissatisfactory")
+		}
+	},
+	leakage10: function(frm) {
+		if(frm.doc.leakage10 == "YES"){
+			frm.set_value('remark10',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark10',"Dissatisfactory")
+		}
+	},
+	leakage11: function(frm) {
+		if(frm.doc.leakage11 == "YES"){
+			frm.set_value('remark11',"Satisfactory")
+		}
+		else{
+			frm.set_value('remark11',"Dissatisfactory")
+		}
+	},
 	refresh:function(frm){
 		frm.trigger('leakage')
 		frm.trigger('leakage2')
 		frm.trigger('leakage3')
 		frm.trigger('leakage4')
 		frm.trigger('leakage5')
+		frm.trigger('leakage6')
+		frm.trigger('leakage7')
+		frm.trigger('leakage8')
+		frm.trigger('leakage9')
+		frm.trigger('leakage10')
+		frm.trigger('leakage11')
 
 	}
 });

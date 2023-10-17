fec00864e9e0b1ad65a74b45d3fabbd55ad931ef
Ubuntu <ubuntu@ip-172-31-44-225.ap-south-1.compute.internal>
2023-10-17 06:32:45 +0000

    fix

---
+++ bindal/bindal/doctype/incoming_material_inspection/incoming_material_inspection.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jyoti and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Incoming Material Inspection', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ bindal/bindal/doctype/incoming_material_inspection/test_incoming_material_inspection.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, jyoti and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestIncomingMaterialInspection(unittest.TestCase):
+	pass

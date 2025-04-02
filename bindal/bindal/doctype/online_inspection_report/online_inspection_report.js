a8bbd6f66472e4e13a5b5108a8f28c1303348158
Gokulnath17 <gokulnath@thirvusoft.co.in>
2025-04-02 06:09:55 +0000

    feat: removed custom in doctype

---
+++ bindal/bindal/doctype/online_inspection_report/online_inspection_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jyoti and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Online Inspection Report", {
+// 	refresh(frm) {
+
+// 	},
+// });

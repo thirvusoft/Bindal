7b5648e459dcd8e8e210004ca9059f657c4dc66d
Gokulnath17 <gokulnath@thirvusoft.co.in>
2025-04-29 04:19:30 +0000

    feat: live changes-> customization

--- bindal/bindal/custom/py/salary_slip.py
+++ bindal/bindal/custom/py/salary_slip.py
@@ -2,4 +2,21 @@ import frappe
 from frappe.model.naming import make_autoname
 
 def create_name(doc, event):
-    doc.name = make_autoname(f"{doc.custom_naming_series}")
+    if doc.employee:
+        branch = frappe.db.get_value("Employee", doc.employee, "branch")
+
+        # Define mapping of branch to naming series
+        naming_series_map = {
+            "Head Office": "HO-SALSLIP26-.####",
+            "Unit-1": "U1-SALSLIP26-.####",
+            "Unit-2": "U2-SALSLIP26-.####",
+            "Unit-3": "U3-SALSLIP26-.####",
+            "Unit-4": "U4-SALSLIP26-.####",
+        }
+
+        # Check if branch is in the mapping
+        if branch in naming_series_map:
+            doc.name = make_autoname(naming_series_map[branch])
+        else:
+            # fallback naming series if branch doesn't match
+            doc.name = make_autoname("SALSLIP26-.####")
